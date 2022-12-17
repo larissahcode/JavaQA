9579a0362c15205aed5b6c16ccc6cba6b44ea754
larissahcode <larissarodfer@gmail,com>
2022-12-17 15:55:45 -0300

    variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class TiposVariaveis {
+public class Tiposvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14; //`Para não ter a possibilidade de mudar o valor, tem que colocar "final" no inicio e a variável em caixa alta
+
+}
 }
